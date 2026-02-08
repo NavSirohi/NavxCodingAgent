f81caddb2ee476fb4518e46e9e4d642d584a56db
Nikhil Sirohi <nikhilsirohi14@gmail.com>
2026-02-08 13:48:29 +0530

    Setup Successful

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+export default function Home() {
+  return (
+  <div className="flex h-screen flex-col justify-center items-center">
+    <Button>Test</Button>
+  </div>
+  );
+}
